ccd521e6753717f320bad1f9e458bf37b4315f0e
Gowtham Devabhakthini <gowthamdevabhakthini1708@gmail.com>
2026-07-12 23:55:31 +0530

    Added Sqrt

---
+++ 0069-Sqrt/solution.c
@@ -0,0 +1,16 @@
+int mySqrt(int x) {
+    if (x == 0) return 0;
+    long left = 1;
+    long right = x;
+    long ans = 0;
+    while (left <= right) {
+        long mid = left + (right - left) / 2;    
+        if (mid * mid <= x) {
+            ans = mid;          
+            left = mid + 1;    
+        } else {
+            right = mid - 1;    
+        }
+    }
+    return ans;
+}
